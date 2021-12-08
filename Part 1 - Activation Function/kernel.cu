594f0e371eb42e8960aec24ce8a7aaaa2a072f5a
ThoenigAdrian <thoenig.adrian@hotmail.com>
2021-12-08 01:43:48 +0100

    indentation

--- Part 1 - Activation Function/kernel.cu
+++ Part 1 - Activation Function/kernel.cu
@@ -15,36 +15,35 @@ int main()
 {
     const int arraySize = 5;
 
-	// Initializing arrays on the CPU
-	float host_z_values[arraySize] = { 1., 2., 3., 4., 5. };
-	float host_activations[arraySize] = { 0 };
+    // Initializing arrays on the CPU
+    float host_z_values[arraySize] = { 1., 2., 3., 4., 5. };
+    float host_activations[arraySize] = { 0 };
 
-	// Caclulating the number of bytes required to store the arrays.
-	const size_t bytes_z_values = arraySize * sizeof(float);
-	const size_t bytes_activations = arraySize * sizeof(float);
+    // Caclulating the number of bytes required to store the arrays.
+    const size_t bytes_z_values = arraySize * sizeof(float);
+    const size_t bytes_activations = arraySize * sizeof(float);
 
-	// Float pointers which will contain the address of the arrays on the GPU.
-	float *device_z_values, *device_activations;
+    // Float pointers which will contain the address of the arrays on the GPU.
+    float *device_z_values, *device_activations;
 
-	// Allocate memory on the GPU
-	cudaMalloc(&device_z_values, bytes_z_values);
-	cudaMalloc(&device_activations, bytes_activations);
+    // Allocate memory on the GPU
+    cudaMalloc(&device_z_values, bytes_z_values);
+    cudaMalloc(&device_activations, bytes_activations);
 
-	// Now that we have allocated memory space and the location is stored in our pointer we can transfer the values from the CPU to the GPU.
-	cudaMemcpy(device_z_values, host_z_values, bytes_z_values, cudaMemcpyHostToDevice);
-
-
-	// Call the kernel which calculates the activations.
-	// 1 = Number of Blocks
-	// arraySize is the number of threads
-	sigmoidActivation << <1, arraySize >> > (device_z_values, device_activations);
+    // Now that we have allocated memory space and the location is stored in our pointer we can transfer the values from the CPU to the GPU.
+    cudaMemcpy(device_z_values, host_z_values, bytes_z_values, cudaMemcpyHostToDevice);
 
 
-	// Copy the results back to the CPU
-	cudaMemcpy(host_activations, device_activations, bytes_z_values, cudaMemcpyDeviceToHost);
+    // Call the kernel which calculates the activations.
+    // 1 = Number of Blocks
+    // arraySize is the number of threads
+    sigmoidActivation << <1, arraySize >> > (device_z_values, device_activations);
+
+
+    // Copy the results back to the CPU
+    cudaMemcpy(host_activations, device_activations, bytes_z_values, cudaMemcpyDeviceToHost);
 	
-        printf("sigmoid({1,2,3,4,5}) = {%f,%f, %f,%f,%f}\n", host_activations[0], host_activations[1], host_activations[2], host_activations[3], host_activations[4]);
-	getchar();
-
-        return 0;
+    printf("sigmoid({1,2,3,4,5}) = {%f,%f, %f,%f,%f}\n", host_activations[0], host_activations[1], host_activations[2], host_activations[3], host_activations[4]);
+    getchar();
+    return 0;
 }
